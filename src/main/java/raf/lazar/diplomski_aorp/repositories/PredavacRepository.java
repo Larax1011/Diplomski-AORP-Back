25e2941246216ed28a7a6a11c8abb579188b1401
Larax1011 <laza98@gmail.com>
2023-12-04 13:45:40 +0100

    Filtering Predmet and Predavac changed from code to db

--- src/main/java/raf/lazar/diplomski_aorp/repositories/PredavacRepository.java
+++ src/main/java/raf/lazar/diplomski_aorp/repositories/PredavacRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import raf.lazar.diplomski_aorp.model.Predavac;
 
+import java.util.List;
+
 @Repository
 public interface PredavacRepository extends JpaRepository<Predavac, Long> {
+
+    List<Predavac> findAllByPredavanjaIsNotEmptyOrVanredniCasoviIsNotEmpty();
+    List<Predavac> findAllByPredavanjaIsNotEmpty();
 
 }
--- src/main/java/raf/lazar/diplomski_aorp/repositories/PredmetRepository.java
+++ src/main/java/raf/lazar/diplomski_aorp/repositories/PredmetRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import raf.lazar.diplomski_aorp.model.Predmet;
 
+import java.util.List;
+
 @Repository
 public interface PredmetRepository extends JpaRepository<Predmet, Long> {
+
+    List<Predmet> findAllByPredavanjaIsNotEmpty();
 }
